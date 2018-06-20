ec7645b9629a54334f169ce34436e65aafd78fd9
avinash <avish1990@gmail.com>
2018-06-20 06:43:16 +0000

    Matrix Transpose updated

--- matrixtranspose.py
+++ matrixtranspose.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python
 def main():
   
-  listofp = []
-  m = [int(x) for x in raw_input("Enter space sepertaed dimensions of list: ").split()]
-  for i in range(0,m[0]):
-    p = [int(x) for x in raw_input("Enter space seperated row %s for list: " % (i+1)).split()]  
-    listofp.append(p)
-  
-  transp = [[listofp[j][i] for j in range(len(listofp))] for i in range(len(listofp[0]))]
-  for i in transp:
+  dimensions = [int(x) for x in raw_input("Enter space separated dimensions of list: ").split()]
+  array = [[int(x) for x in raw_input("Enter space separated row %s for list: " % (i+1)).split()] for i in range(0,dimensions[0])]
+  trans_array = [[array[j][i] for j in range(len(array))] for i in range(len(array[0]))]
+  for i in trans_array:
     print ' '.join(map(str, i)) 
 main()
